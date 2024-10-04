240ee5f9ec3b36ff033c0721e0edaff740d8d6a9
gasta <gaston.e.coria@gmail.com>
2024-10-04 19:51:25 -0300

    add  pokeapi client

--- internal/pokeapi/types_location_area_resp.go
+++ internal/pokeapi/types_location_area_resp.go
@@ -1,6 +1,6 @@
 package pokeapi
 
-type LocationAreResp struct {
+type LocationAreaResp struct {
 	Count    int     `json: "count, omitempty"`
 	Next     *string `json: "next,omitempty"`
 	Previous *string `json: "previous,omitempty"`
